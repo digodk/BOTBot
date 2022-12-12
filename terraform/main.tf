935c654baf5713f8ae216331fcd6a691e3fa34c3
digodk <20893375+digodk@users.noreply.github.com>
2022-12-12 20:49:07 -0300

    WIP - Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,38 @@ provider "aws" {
 
 resource "aws_api_gateway_rest_api" "telegram-webhook" {
     name = "telegram"
-    endpoint_configuration = [
-              {
-                "types": [
-                  "REGIONAL"
-                ],
-                "vpc_endpoint_ids": []
-              }]
-    id = "ddmmlg41i3"
 }
+
+resource "aws_api_gateway_resource" "config-bot" {
+    rest_api_id = aws_api_gateway_rest_api.telegram-webhook.id
+    parent_id = aws_api_gateway_rest_api.telegram-webhook.root_resource_id
+    path_part = "config-bot"
+}
+
+resource "aws_api_gateway_method" "config-bot-post" {
+  rest_api_id = aws_api_gateway_rest_api.telegram-webhook.id
+  resource_id = aws_api_gateway_resource.config-bot.id
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_iam_role" "apiLambdaIntegrationRole" {
+  
+}
+
+resource "aws_api_gateway_integration" "config-bot-post-integration" {
+  rest_api_id = aws_api_gateway_rest_api.telegram-webhook.id
+  resource_id = aws_api_gateway_resource.config-bot.id
+  http_method = aws_api_gateway_method.config-bot-post.http_method
+  type = "AWS"
+  integration_http_method = "POST"
+  credentials = "arn:aws:iam::020664526196:role/AWSRoleForAPIGateway"
+  request_parameters = {
+              "integration.request.header.Content-Type": "'application/x-www-form-urlencoded'"
+            }
+  request_templates = {
+              "application/json": "Action=SendMessage\u0026MessageBody=$input.body"
+            }
+  uri = "arn:aws:apigateway:sa-east-1:sqs:path/020664526196/telegram-updates"
+}
+
