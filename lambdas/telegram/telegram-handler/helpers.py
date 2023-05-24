bb1902859d4c7a7f3e6fd29547f9b056effd0cbc
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-05-24 01:06:25 -0300

    docstrings

--- lambdas/telegram/telegram-handler/helpers.py
+++ lambdas/telegram/telegram-handler/helpers.py
@@ -14,10 +14,34 @@ table = dynamodb.Table('topic_subscribers')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def valid_topic(topic):
+    """
+    Validate the topic to ensure it only contains alphanumeric 
+    characters or underscores and is not longer than 32 characters.
+
+    Args:
+    topic (str): The topic to validate
+
+    Returns:
+    bool: True if the topic is valid, False otherwise.
+    """
+
     return re.match(r'^[a-z0-9_]{1,32}$', topic)
 
+
 def add_subscriber(topic, chat_id):
+    """
+    Adds a chat id subscription to a topic in the DynamoDB subscriptions table.
+
+    Args:
+    topic (str): The topic to subscribe to
+    chat_id (str): The chat id to add to the subscription
+
+    Returns:
+        A json response from the call made to AWS using boto3
+    """
+
     try:
         response = table.get_item(
             Key={
@@ -48,6 +72,17 @@ def add_subscriber(topic, chat_id):
 
 
 def subscribe_to_topic(topic, chat_id):
+    """
+    Subscribes a user to a topic.
+
+    Args:
+    topic (str): The topic to subscribe to
+    chat_id (str): The chat id to add to the subscription
+
+    Returns:
+    The telegram reponse to be sent to the user
+    """
+
     topic = topic.lstrip().lower()
     if valid_topic(topic):
         add_subscriber(str(topic), str(chat_id))
@@ -64,6 +99,18 @@ def subscribe_to_topic(topic, chat_id):
 
 
 def unsubscribe_from_topic(topic, chat_id, remove_subscriber):
+    """
+    Unsubscribe a user from a topic.
+
+    Args:
+    topic (str): The topic to unsubscribe from
+    chat_id (str): The chat id to remove from the subscription
+    remove_subscriber (func): A function to remove the subscriber
+
+    Returns:
+    str: A message indicating the result of the unsubscription attempt
+    """
+
     topic = topic.lstrip().lower()
     if valid_topic(topic):
         remove_subscriber(topic, chat_id)
@@ -76,6 +123,17 @@ def unsubscribe_from_topic(topic, chat_id, remove_subscriber):
 
 
 def send_telegram_message(chatId, message, botName='configurator'):
+    """
+    Publish a message to the SQS queue for sending a message to a Telegram user.
+
+    Args:
+    chatId (str): The id of the chat to send the message to
+    message (str): The message to send
+    botName (str, optional): The name of the bot to send the message. Defaults to 'configurator'.
+
+    Returns:
+    dict: The response from the SQS send_message function
+    """
 
     logger.info(
         f'Publicando na fila de envio do telegram a mensagem {message} para chatId {chatId}')
@@ -83,11 +141,22 @@ def send_telegram_message(chatId, message, botName='configurator'):
         'botName': botName,
         'textMessage': message
     }
-    response = send_sqs_message([chatId], payload, 'send-message-queue')
-    return response
+    return send_sqs_message([chatId], payload, 'send-message-queue')
 
 
 def broadcast_message(chatId, payload, topic):
+    """
+    Broadcast a message to all subscribers of a topic.
+
+    Args:
+    chatId (str): The id of the chat that the message originates from
+    payload (dict): The message to send
+    topic (str): The topic to send the message to
+
+    Returns:
+    dict: The response from the SQS send_message function
+    """
+
     logger.info(
         f'Divulgando mensagem {payload} para o tópico {topic}, vinda do chat {chatId}')
 
@@ -105,7 +174,18 @@ def broadcast_message(chatId, payload, topic):
 
 
 def get_subscribers(topic):
-    logger.info(f'enviando request para os subscribers do seguinte tópico: {topic}')
+    """
+    Get a list of subscribers for a topic.
+
+    Args:
+    topic (str): The topic to get subscribers for
+
+    Returns:
+    list: A list of subscribers
+    """
+
+    logger.info(
+        f'enviando request para os subscribers do seguinte tópico: {topic}')
     try:
         response = table.query(
             KeyConditionExpression=Key('topic').eq(topic)
@@ -120,6 +200,18 @@ def get_subscribers(topic):
 
 
 def send_sqs_message(recipients, payload, sqsName):
+    """
+    Send a message to an SQS queue.
+
+    Args:
+    recipients (list): The recipients of the message
+    payload (dict): The message to send
+    sqsName (str): The name of the SQS queue to send the message to
+
+    Returns:
+    dict: The response from the SQS send_message function
+    """
+
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
