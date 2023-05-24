83d2159820aa8a8d1f1b14b0501bfe9121c50b67
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-05-24 17:20:26 -0300

    correcao de variavel

--- variables.tf
+++ variables.tf
@@ -3,5 +3,4 @@ locals {
   subscribers_table_name = "topic_subscribers"
   telegram_api_name = "telegram"
   aws_account_id = "020664526196"
-  configurator_bot_token = var.configurator_bot_token
 }
