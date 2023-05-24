7d332d31650af2105a3c618799948dade00392e8
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-05-24 18:13:15 -0300

    ajuste para importar corretamente variável de ambiente

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
+variable "configurator_bot_token" {
+  description = "The token for the configurator bot"
+}
+
 locals {
   region = "us-east-1"
   subscribers_table_name = "topic_subscribers"
   telegram_api_name = "telegram"
   aws_account_id = "020664526196"
+  configurator_bot_token = var.configurator_bot_token
 }
