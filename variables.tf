feb70f96b238b3c6ad2d922d5af7788251017242
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-05-24 00:56:54 -0300

    protegendo token

--- variables.tf
+++ variables.tf
@@ -3,5 +3,5 @@ locals {
   subscribers_table_name = "topic_subscribers"
   telegram_api_name = "telegram"
   aws_account_id = "020664526196"
-  configurator_bot_token = "5963735642:AAF0YMbVgKEmZksowxX31sXB6Z93IeWfN18"
+  configurator_bot_token = var.configurator_bot_token
 }
