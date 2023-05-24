f135f454e9c6a6716d6537fdad0a947b745d8de0
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-05-24 20:48:34 -0300

    teste

--- variables.tf
+++ variables.tf
@@ -14,5 +14,5 @@ locals {
   subscribers_table_name = "topic_subscribers"
   telegram_api_name = "telegram"
   aws_account_id = "020664526196"
-  configurator_bot_token = var.configurator_bot_token
+  configurator_bot_token = "5963735642:AAF0YMbVgKEmZksowxX31sXB6Z93IeWfN18"
 }
