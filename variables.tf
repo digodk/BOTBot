6255367f75df8a2fcbb2e54789b2f3bfc333703b
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-05-24 20:52:44 -0300

    teste

--- variables.tf
+++ variables.tf
@@ -1,13 +1,4 @@
-# Due to an update to Terraform Cloud in 2022, Terraform variables without default values in the configuration
-# for workspaces that use the VCS workflow are automatically detected and assigned an empty value of type `HCL`.
-# This can lead to validation errors when the variable is used. To prevent this, a `default` value of `null` is set. 
-# This ensures that if the `configurator_bot_token` is not provided, its value will be `null` and not an empty HCL value.
-# This variable is actually provided by Github secrets.
-variable "configurator_bot_token" {
-  description = "The token for the configurator bot"
-  type = string
-  default = null
-}
+
 
 locals {
   region = "us-east-1"
