4e94faaee3ed716ddab4d971c0bcb1a6b72d3af5
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-05-24 21:37:16 -0300

    teste

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
-
+variable "config_bot_token" {
+  description = "Token for the configurator bot"
+}
 
 locals {
   region = "us-east-1"
   subscribers_table_name = "topic_subscribers"
   telegram_api_name = "telegram"
   aws_account_id = "020664526196"
-  configurator_bot_token = "5963735642:AAF0YMbVgKEmZksowxX31sXB6Z93IeWfN18"
+  configurator_bot_token = var.config_bot_token
 }
